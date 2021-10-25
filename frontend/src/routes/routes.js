8a849f61fb2c0e95cb2e4c6f16c51fa9d95eedb5
AhmedMuhaameed <ahmed.m.abdelrahman0@gmail.com>
2021-10-25 17:21:34 +0200

    Authentication Backend & directory Strucure for Frontend

---
+++ frontend/src/routes/routes.js
@@ -0,0 +1,8 @@
+import { Route } from "react-router-dom"; 
+export function Routes(){
+    return(
+        <div>
+            {/* <Route path="/" exact component={ListProducts}/> */}
+        </div>
+    )
+}
---
+++ frontend/src/services/productService.js
@@ -0,0 +1,7 @@
+import http from "../utils/http";
+
+const ProductService = {
+    // getAll
+};
+
+export default ProductService;
