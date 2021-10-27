dd37e091d05b3ce94f41e603700e2c3eb5491b30
AhmedMuhaameed <ahmed.m.abdelrahman0@gmail.com>
2021-10-27 18:40:38 +0200

    Added Cart Page

--- frontend/src/actions/types.js
+++ frontend/src/actions/types.js
@@ -3,3 +3,6 @@ export const GET_PRODUCT = "GET_PRODUCT";
 export const POST_PRODUCTS = "POST_PRODUCTS";
 export const UPDATE_PRODUCTS = "UPDATE_PRODUCTS";
 export const DELETE_PRODUCTS = "DELETE_PRODUCTS";
+
+export const CART_ADD_ITEM = "CART_ADD_ITEM";
+export const CART_REMOVE_ITEM = "CART_REMOVE_ITEM";
